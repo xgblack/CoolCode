2ea71df1ba8ba7344444ed9ff42eecebce889086
xgblack <xgblack@qq.com>
2023-12-19 01:00:48 +0800

    init

---
+++ src/test/java/com/xgblack/cool/CoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xgblack.cool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
