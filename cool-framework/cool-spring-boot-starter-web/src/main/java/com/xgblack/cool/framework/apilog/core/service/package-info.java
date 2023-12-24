d420a19ff2c2ebdeff801dbde64aeebbbf387591
xgblack <xgblack@qq.com>
2023-12-24 14:09:02 +0800

    feat(web): 【组件】web模块暂时完成

---
+++ cool-framework/cool-spring-boot-starter-web/src/main/java/com/xgblack/cool/framework/apilog/core/service/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * @author xg BLACK
+ * @date 2023/12/24 13:57
+ */
+
+package com.xgblack.cool.framework.apilog.core.service;
---
+++ cool-framework/cool-spring-boot-starter-web/src/main/java/com/xgblack/cool/framework/xss/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * @author xg BLACK
+ * @date 2023/12/24 14:01
+ */
+
+package com.xgblack.cool.framework.xss;
