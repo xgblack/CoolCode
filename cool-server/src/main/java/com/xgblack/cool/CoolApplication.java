c01916d70f0ad9ce8925544587dd6f629cf6b348
xgblack <xgblack@qq.com>
2023-12-25 10:01:24 +0800

    refactor(project): 【优化】web模块优化

--- cool-server/src/main/java/com/xgblack/cool/CoolApplication.java
+++ cool-server/src/main/java/com/xgblack/cool/CoolApplication.java
@@ -1,10 +1,8 @@
 package com.xgblack.cool;
 
-import com.feiniaojin.gracefulresponse.EnableGracefulResponse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@EnableGracefulResponse
 @SpringBootApplication
 public class CoolApplication {
 
