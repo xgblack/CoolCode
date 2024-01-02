1760858764f4c1810accd34285aab61dafce3852
xgblack <xgblack@qq.com>
2024-01-02 11:38:01 +0800

    feat(mybatis): 修改FlexGlobalConfig配置方式

--- cool-framework/cool-spring-boot-starter-mybatis-flex/src/main/java/com/xgblack/cool/framework/mybatis/config/CoolMybatisAutoConfiguration.java
+++ cool-framework/cool-spring-boot-starter-mybatis-flex/src/main/java/com/xgblack/cool/framework/mybatis/config/CoolMybatisAutoConfiguration.java
@@ -1,6 +1,7 @@
 package com.xgblack.cool.framework.mybatis.config;
 
 import com.mybatisflex.core.FlexGlobalConfig;
+import com.mybatisflex.spring.boot.MyBatisFlexCustomizer;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 
 /**
@@ -9,15 +10,16 @@ import org.springframework.boot.autoconfigure.AutoConfiguration;
  */
 @AutoConfiguration
 //@MapperScan(value = "${cc.info.base-package}", annotationClass = Mapper.class)
-public class CoolMybatisAutoConfiguration {
+public class CoolMybatisAutoConfiguration implements MyBatisFlexCustomizer {
 
-    public void config() {
-        //设置逻辑删除默认值
-        FlexGlobalConfig globalConfig = FlexGlobalConfig.getDefaultConfig();
+
+    @Override
+    public void customize(FlexGlobalConfig globalConfig) {
+        //一些列的初始化配置
+
         //设置数据库正常时的值
         globalConfig.setNormalValueOfLogicDelete(false);
         //设置数据已被删除时的值
         globalConfig.setDeletedValueOfLogicDelete(true);
     }
-
 }
