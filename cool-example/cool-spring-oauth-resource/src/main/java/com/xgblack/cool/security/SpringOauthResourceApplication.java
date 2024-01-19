22dc32950f651fa99230c10ab6a7b68bf0b415ca
xgblack <xgblack@qq.com>
2024-01-19 12:02:30 +0800

    feat 🚀(security): Build a resource server

---
+++ cool-example/cool-spring-oauth-resource/src/main/java/com/xgblack/cool/security/SpringOauthResourceApplication.java
@@ -0,0 +1,16 @@
+package com.xgblack.cool.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <a href="https://blog.51cto.com/u_15268610/6790211">Spring Authorization Server （四）资源服务器搭建</a>
+ * @author <a href="https://www.xgblack.cn">xg black</a>
+ */
+
+@SpringBootApplication
+public class SpringOauthResourceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringOauthResourceApplication.class,args);
+    }
+}
---
+++ cool-example/cool-spring-oauth-resource/src/main/java/com/xgblack/cool/security/controller/MessagesController.java
@@ -0,0 +1,31 @@
+package com.xgblack.cool.security.controller;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author <a href="https://www.xgblack.cn">xg black</a>
+ */
+
+@RestController
+public class MessagesController {
+
+    @GetMapping("/messages1")
+    public String getMessages1() {
+        return " hello Message 1";
+    }
+
+    @GetMapping("/messages2")
+    @PreAuthorize("hasAuthority('SCOPE_profile')")
+    public String getMessages2() {
+        return " hello Message 2";
+    }
+
+    @GetMapping("/messages3")
+    @PreAuthorize("hasAuthority('SCOPE_Message')")
+    public String getMessages3() {
+        return " hello Message 3";
+    }
+
+}
