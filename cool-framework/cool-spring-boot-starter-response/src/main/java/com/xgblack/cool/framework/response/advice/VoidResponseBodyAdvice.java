de51f2f78fb448d0660a9e770012171cbcd90ccf
xgblack <xgblack@qq.com>
2024-02-22 15:45:23 +0800

    🐛fix(response): 修复void返回值不统一包装的问题

--- cool-framework/cool-spring-boot-starter-response/src/main/java/com/xgblack/cool/framework/response/advice/VoidResponseBodyAdvice.java
+++ cool-framework/cool-spring-boot-starter-response/src/main/java/com/xgblack/cool/framework/response/advice/VoidResponseBodyAdvice.java
@@ -3,11 +3,13 @@ package com.xgblack.cool.framework.response.advice;
 import com.xgblack.cool.framework.common.response.api.ResponseFactory;
 import jakarta.annotation.Resource;
 import org.springframework.core.MethodParameter;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
 import java.util.Objects;
@@ -17,8 +19,9 @@ import java.util.Objects;
  *
  * @author <a href="https://www.xgblack.cn">xg black</a>
  */
-//@ControllerAdvice
-//@Order(value = 1000)
+
+@ControllerAdvice
+@Order(value = 1000)
 public class VoidResponseBodyAdvice implements ResponseBodyAdvice<Object> {
 
     @Resource
@@ -32,20 +35,12 @@ public class VoidResponseBodyAdvice implements ResponseBodyAdvice<Object> {
      * @return 是否对这种返回值进行处理
      */
     @Override
-    public boolean supports(MethodParameter methodParameter,
-                            Class<? extends HttpMessageConverter<?>> clazz) {
-
-        return Objects.requireNonNull(methodParameter.getMethod()).getReturnType().equals(Void.TYPE)
-                && MappingJackson2HttpMessageConverter.class.isAssignableFrom(clazz);
+    public boolean supports(MethodParameter methodParameter, Class<? extends HttpMessageConverter<?>> clazz) {
+        return Objects.requireNonNull(methodParameter.getMethod()).getReturnType().equals(Void.TYPE) && MappingJackson2HttpMessageConverter.class.isAssignableFrom(clazz);
     }
 
     @Override
-    public Object beforeBodyWrite(Object body,
-                                  MethodParameter returnType,
-                                  MediaType selectedContentType,
-                                  Class<? extends HttpMessageConverter<?>> selectedConverterType,
-                                  ServerHttpRequest request,
-                                  ServerHttpResponse response) {
+    public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType, Class<? extends HttpMessageConverter<?>> selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
         return responseFactory.newSuccessInstance();
     }
 }
