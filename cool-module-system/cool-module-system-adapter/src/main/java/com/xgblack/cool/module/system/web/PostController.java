ae46fe3af4c16f5473e90e2a8af7681874d7ec25
xgblack <xgblack@qq.com>
2024-03-09 23:17:14 +0800

    ✨feat(rbac): 新增 岗位部分代码

---
+++ cool-module-system/cool-module-system-adapter/src/main/java/com/xgblack/cool/module/system/web/PostController.java
@@ -0,0 +1,20 @@
+package com.xgblack.cool.module.system.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author <a href="https://www.xgblack.cn">xg black</a>
+ */
+
+@RestController
+@RequestMapping("/system/post")
+@Validated
+@RequiredArgsConstructor
+public class PostController {
+
+
+
+}
