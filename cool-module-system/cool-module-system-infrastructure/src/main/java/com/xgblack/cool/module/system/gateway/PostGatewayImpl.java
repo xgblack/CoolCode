f399dfb70cce844f55c5d04efe1d31cbaded3ee9
xgblack <xgblack@qq.com>
2024-03-11 11:02:19 +0800

    ✨feat(rbac): 新增 岗位增删改查接口

--- cool-module-system/cool-module-system-infrastructure/src/main/java/com/xgblack/cool/module/system/gateway/PostGatewayImpl.java
+++ cool-module-system/cool-module-system-infrastructure/src/main/java/com/xgblack/cool/module/system/gateway/PostGatewayImpl.java
@@ -1,9 +1,19 @@
 package com.xgblack.cool.module.system.gateway;
 
+import com.mybatisflex.core.query.QueryChain;
+import com.xgblack.cool.framework.common.pojo.dto.PageResult;
+import com.xgblack.cool.module.system.convertor.PostConvertor;
+import com.xgblack.cool.module.system.domain.company.post.Post;
 import com.xgblack.cool.module.system.domain.gateway.PostGateway;
+import com.xgblack.cool.module.system.dto.company.post.PostPageQry;
+import com.xgblack.cool.module.system.gateway.database.dataobject.table.PostTableDef;
+import com.xgblack.cool.module.system.gateway.database.mapper.PostMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.dromara.hutool.core.text.StrUtil;
 import org.springframework.stereotype.Component;
+
+import java.util.Objects;
 
 /**
  * @author <a href="https://www.xgblack.cn">xg black</a>
@@ -13,4 +23,39 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 public class PostGatewayImpl implements PostGateway {
+
+    private final PostMapper postMapper;
+
+    private final PostConvertor convertor = PostConvertor.INSTANCE;
+
+
+    @Override
+    public void insert(Post entity) {
+        postMapper.insertSelective(convertor.toDataObject(entity));
+    }
+
+    @Override
+    public void update(Post entity) {
+        postMapper.update(convertor.toDataObject(entity));
+    }
+
+    @Override
+    public void delete(Long id) {
+        postMapper.deleteById(id);
+    }
+
+    @Override
+    public Post getById(Long id) {
+        return postMapper.selectOneWithRelationsByIdAs(id, Post.class);
+    }
+
+    @Override
+    public PageResult<Post> page(PostPageQry qry) {
+        return PageResult.of(QueryChain.of(postMapper)
+                .from(PostTableDef.POST)
+                .and(PostTableDef.POST.CODE.like(qry.getCode(), StrUtil.isNotBlank(qry.getCode())))
+                .and(PostTableDef.POST.NAME.like(qry.getName(), StrUtil.isNotBlank(qry.getName())))
+                .and(PostTableDef.POST.STATUS.eq(qry.getStatus(), Objects.nonNull(qry.getStatus())))
+                .pageAs(qry.buildPage(), Post.class));
+    }
 }
